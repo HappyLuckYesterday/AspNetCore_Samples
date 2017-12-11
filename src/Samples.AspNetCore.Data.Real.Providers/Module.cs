7e36eece414eba4226af48e49fc3d83de646b7ee
Gennady Verdel <godrose@gmail.com>
2017-12-11 09:35:15 +0200

    Added DebugWithReal configuration
    Switch between configurations to see how
    Different modules are loaded dynamically

---
+++ src/Samples.AspNetCore.Data.Real.Providers/Module.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Samples.AspNetCore.Data.Contracts.Providers;
+using Solid.Practices.Modularity;
+
+namespace Samples.AspNetCore.Data.Real.Providers
+{
+    class Module : ICompositionModule<IServiceCollection>
+    {
+        public void RegisterModule(IServiceCollection dependencyRegistrator)
+        {
+            dependencyRegistrator.AddSingleton<IPersonProvider, PersonProvider>();
+        }
+    }
+}
---
+++ src/Samples.AspNetCore.Data.Real.Providers/PersonProvider.cs
@@ -0,0 +1,20 @@
+﻿using Samples.AspNetCore.Data.Contracts.Dto;
+using Samples.AspNetCore.Data.Contracts.Providers;
+
+namespace Samples.AspNetCore.Data.Real.Providers
+{
+    class PersonProvider : IPersonProvider
+    {
+        PersonDto[] IPersonProvider.GetPersons()
+        {
+            return new PersonDto[]
+            {
+                new PersonDto
+                {
+                    FirstName = "Real Name",
+                    LastName = "Real Surname"
+                }
+            };
+        }
+    }
+}
