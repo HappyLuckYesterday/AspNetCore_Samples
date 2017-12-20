9747023b53a81423b4ac735b347b22dab65a99e3
Gennady Verdel <godrose@gmail.com>
2017-12-20 18:38:04 +0200

    Using Moq instead of LightMock as Fake Factory

---
+++ src/Samples.AspNetCore.Data.Fake.ProviderBuilders.Moq/Module.cs
@@ -0,0 +1,15 @@
+﻿using Attest.Fake.Core;
+using Attest.Fake.Moq;
+using Solid.Practices.Modularity;
+
+namespace Samples.AspNetCore.Data.Fake.ProviderBuilders
+{
+    public class Module : IPlainCompositionModule
+    {
+        public void RegisterModule()
+        {
+            FakeFactoryContext.Current = new FakeFactory();
+            ConstraintFactoryContext.Current = new ConstraintFactory();
+        }
+    }
+}
---
+++ src/Samples.AspNetCore.Data.Fake.ProviderBuilders.Moq/PersonProviderBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Attest.Fake.Setup.Contracts;
+using LogoFX.Client.Data.Fake.ProviderBuilders;
+using Samples.AspNetCore.Data.Contracts.Dto;
+using Samples.AspNetCore.Data.Contracts.Providers;
+
+namespace Samples.AspNetCore.Data.Fake.ProviderBuilders
+{
+    public class PersonProviderBuilder : FakeBuilderBase<IPersonProvider>
+    {
+        private readonly List<PersonDto> _persons = new List<PersonDto>();
+
+        public static PersonProviderBuilder CreateBuilder()
+        {
+            return new PersonProviderBuilder();
+        }
+
+        protected override IServiceCall<IPersonProvider>
+            CreateServiceCall(IHaveNoMethods<IPersonProvider> serviceCallTemplate)
+        {
+            var setup = serviceCallTemplate
+                .AddMethodCallWithResult(
+                    t => t.GetPersons(),
+                    r => r.Complete(_persons.ToArray()));
+            return setup;
+        }
+
+        public void WithPersons(IEnumerable<PersonDto> persons)
+        {
+            _persons.Clear();
+            _persons.AddRange(persons);
+        }
+    }
+}
